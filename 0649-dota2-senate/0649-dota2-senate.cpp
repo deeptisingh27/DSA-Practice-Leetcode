04ef5abcce39d7ea099fde2eef4d4e9f8ffd1ec3
Deepti Singh <deeptisingh2710@gmail.com>
2026-06-21 16:42:48 +0530

    Time: 0 ms (100%), Space: 11.6 MB (53.38%) - LeetHub

--- 0649-dota2-senate/0649-dota2-senate.cpp
+++ 0649-dota2-senate/0649-dota2-senate.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    /*
     bool removeSenator(string &senate, char ch, int idx) {
         bool loopAround = false;
         
@@ -20,9 +21,11 @@ public:
         
         return loopAround;
     }
+    */
 
     string predictPartyVictory(string senate) {
 
+        /*
         int R_Count = count(begin(senate), end(senate), 'R');
         int D_Count = senate.length() - R_Count;        
         
@@ -48,5 +51,39 @@ public:
         }
         
         return R_Count == 0 ? "Dire" : "Radiant";
+        */
+
+
+
+        int n = senate.length();
+        
+        queue<int> queR;
+        queue<int> queD;
+        
+        for(int i = 0; i<n; i++) {
+            if(senate[i] == 'R')
+                queR.push(i);
+            else
+                queD.push(i);
+        }
+        
+        while(!queR.empty() && !queD.empty()) {
+            
+            int R_idx = queR.front(); queR.pop();
+            int D_idx = queD.front(); queD.pop();
+            
+            if(R_idx < D_idx) {
+                queR.push(R_idx+n);
+                //Why +n ?
+                //Do this dry run on "RDD"
+            } else {
+                queD.push(D_idx+n);
+                //Why +n ?
+                //Do this dry run on "RDD"
+            }
+            
+        }
+        
+        return queR.empty() ? "Dire" : "Radiant";
     }
 };
