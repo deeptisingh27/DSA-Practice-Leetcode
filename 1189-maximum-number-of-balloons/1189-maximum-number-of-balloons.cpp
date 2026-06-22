99045f14fe7e8e2eb62ad5d3f0cbe69db149de77
Deepti Singh <deeptisingh2710@gmail.com>
2026-06-22 13:14:12 +0530

    Time: 2 ms (19.9%), Space: 9.1 MB (39.61%) - LeetHub

---
+++ 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int maxNumberOfBalloons(string text) {
+        
+        int n = text.size();
+
+        vector<int> freq(26, 0);
+
+        for(char ch : text){
+            freq[ch-'a']++;
+        }
+
+        int count = INT_MAX;
+        
+        count = min(count, freq['b'-'a']);
+        count = min(count, freq['a'-'a']);
+        count = min(count, freq['l'-'a'] / 2);
+        count = min(count, freq['o'-'a'] / 2);
+        count = min(count, freq['n'-'a']);
+
+        return count;
+    }
+};
