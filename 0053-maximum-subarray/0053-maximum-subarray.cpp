ef55c91b5b30d75540625debd97ae21f2125ed5b
Deepti Singh <deeptisingh2710@gmail.com>
2026-06-30 02:45:56 +0530

    Time: 0 ms (100%), Space: 71.7 MB (80.72%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -2,27 +2,6 @@ class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
 
-        /*
-
-        //BRUTE FORCE 
-        //T.C = O(n^2)
-
-        int maxSum = INT_MIN;
-
-        for(int start = 0 ; start<nums.size() ; start++){
-            int currSum=0;
-
-            for(int end = start ; end<nums.size() ; end++){
-                currSum += nums[end];
-                maxSum = max(maxSum,currSum);
-            }
-        }
-
-        return maxSum;
-
-        */
-
-
         // max./min. subarray -> KADANE'S ALGORITHM 
         // T.C = O(n) , S.C = O(n)
 
