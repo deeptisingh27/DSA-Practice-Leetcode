ec9d74a749fc1e7a73eebd9ed007e9cfd2095d21
Deepti Singh <deeptisingh2710@gmail.com>
2026-07-19 14:34:08 +0530

    Time: 0 ms (100%), Space: 9.2 MB (84.28%) - LeetHub

---
+++ 0316-remove-duplicate-letters/0316-remove-duplicate-letters.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    string removeDuplicateLetters(string s) {
+        //T.C = O(n) , S.C = O(26) = O(1)
+
+        vector<int> lastIndex(26);
+        vector<bool> taken(26, false);
+        
+        string ans;
+
+        for(int i=0 ; i<s.length() ; i++){
+            lastIndex[s[i]-'a'] = i;
+        }
+
+        for(int i=0 ; i<s.length() ; i++){
+            char ch = s[i];
+            int idx = ch-'a';
+
+            if(taken[idx]) continue;
+
+            while(ans.length() > 0 && ans.back() > ch && lastIndex[ans.back() - 'a'] > i){
+                taken[ans.back() - 'a'] = false;
+                ans.pop_back();
+            }
+
+            ans.push_back(ch);
+            taken[idx] = true;
+        }
+
+        return ans;
+    }
+};
