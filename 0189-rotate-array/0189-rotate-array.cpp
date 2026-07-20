9ba8b0726a708a6c25a3d0bcd7cddd461f097e0e
Deepti Singh <deeptisingh2710@gmail.com>
2026-07-20 19:49:15 +0530

    Time: 0 ms (100%), Space: 29.5 MB (69.88%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -1,5 +1,18 @@
 class Solution {
 public:
+    //reverse function using two-pointer technique
+    void reverseArr(vector<int>& nums, int i, int j){
+
+        while (i < j){
+            int temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+            
+            i++;
+            j--;
+        }
+    }
+
     void rotate(vector<int>& nums, int k) {
 
         //T.C. = O(n) , S.C. = O(1)
@@ -7,8 +20,16 @@ public:
         int n = nums.size();
         k %= n; //normalizing k
 
-        reverse(nums.begin(), nums.end()); //Reverse the entire array
+        /*
+        //using built in reverse function
+
+        reverse(nums.begin(), nums.end());       //Reverse the entire array
         reverse(nums.begin(), nums.begin() + k); //Reverse first k elements
-        reverse(nums.begin() + k, nums.end()); //Reverse remaining elements
+        reverse(nums.begin() + k, nums.end());   //Reverse remaining elements
+        */
+
+        reverseArr(nums, 0, n-1);
+        reverseArr(nums, 0, k-1);
+        reverseArr(nums, k, n-1);
     }
 };
