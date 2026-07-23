1cb8ee4c176ee65e9bbe852469ddcfdda453b37f
Deepti Singh <deeptisingh2710@gmail.com>
2026-07-24 00:28:08 +0530

    Time: 46 ms (38.86%), Space: 8.1 MB (40.13%) - LeetHub

--- 1680-concatenation-of-consecutive-binary-numbers/1680-concatenation-of-consecutive-binary-numbers.cpp
+++ 1680-concatenation-of-consecutive-binary-numbers/1680-concatenation-of-consecutive-binary-numbers.cpp
@@ -6,10 +6,18 @@ public:
         //T.C = O(n) , S.C = O(1)
 
         long long ans = 0;
+        int digits = 0;
 
         for(int num=1 ; num<=n ; num++){
-            //calculates the binary bit length of 'num'
-            int digits = log2(num) + 1;
+            
+            /*
+            int digits = log2(num) + 1; //calculates the binary bit length of 'num'
+            */
+
+            //if 'num' is a power of 2, its binary representation needs 1 additional bit
+            if((num & (num-1)) == 0){
+                digits++;
+            }
             
             //shift 'ans' left by 'digits' bits to make room for 'num' & add 'num'
             ans = ((ans << digits) % MOD + num) % MOD;
