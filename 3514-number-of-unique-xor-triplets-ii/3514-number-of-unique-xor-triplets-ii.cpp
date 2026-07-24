4f702a5e6a5b738ad89f986b0884c716e643b009
Deepti Singh <deeptisingh2710@gmail.com>
2026-07-24 14:29:53 +0530

    Time: 587 ms (67.98%), Space: 35.2 MB (82.58%) - LeetHub

--- 3514-number-of-unique-xor-triplets-ii/3514-number-of-unique-xor-triplets-ii.cpp
+++ 3514-number-of-unique-xor-triplets-ii/3514-number-of-unique-xor-triplets-ii.cpp
@@ -3,7 +3,10 @@ public:
     int uniqueXorTriplets(vector<int>& nums) {
         int n = nums.size();
 
-        //store all pair XORs
+        /*
+        //APPROACH 1
+
+        //store all unique pair XORs
         unordered_set<int> s1;
         for(int i=0 ; i<n ; i++){
             for(int j=i ; j<n ; j++){
@@ -20,5 +23,45 @@ public:
         }
 
         return s2.size();
+        */
+
+
+        //APPROACH 2
+
+        int maxEl = *max_element(nums.begin(), nums.end());
+
+        int T = 1; //T is the smallest power of 2 strictly greater than maxEl
+        while(T <= maxEl){
+            T = T << 1; // T = T*2;
+        }
+
+        vector<bool> s1(T, false); //XOR pair values set to true
+        vector<bool> s2(T, false); //XOR triplet values set to true
+
+        //mark all achievable pair XOR values in s1
+        for(int i=0 ; i<n ; i++){
+            for(int j=i ; j<n ; j++){
+                s1[nums[i] ^ nums[j]] = true;
+            }
+        }
+
+        //triplet XOR   
+        for(int i=0 ; i<T ; i++){
+            if(s1[i] == true){
+                for(int &num : nums){
+                    s2[i ^ num] = true;
+                }
+            }
+        }
+
+        //count total number of unique valid triplet XOR values marked in s2
+        int count = 0; 
+        for(int i=0 ; i<T ; i++){
+            if(s2[i] == true){
+                count++;
+            }
+        }
+
+        return count;
     }
 };
