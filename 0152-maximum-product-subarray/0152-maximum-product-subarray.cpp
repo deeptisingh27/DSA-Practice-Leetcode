da35e7a9abf456830c0cffb3fc3e337925457ea8
Deepti Singh <deeptisingh2710@gmail.com>
2026-07-26 12:08:38 +0530

    Time: 0 ms (100%), Space: 17.8 MB (23.97%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int maxProduct(vector<int>& nums) {
-
+        /*
         //brute force
         //T.C = O(n^2) , S.C = O(1)
         
@@ -19,5 +19,31 @@ public:
         }
 
         return ans;
+        */
+
+
+        //KADANE'S ALGORITHM
+        // T.C = O(n) , S.C = O(1)
+
+        int currMax = nums[0];
+        int currMin = nums[0];
+        int maxProd = nums[0];
+
+        for (int i = 1; i < nums.size(); i++) {
+            int num = nums[i];
+
+            // Multiplying by a negative number flips max and min
+            if (num < 0) {
+                swap(currMax, currMin);
+            }
+
+            // Either start a new subarray at num, or extend previous product
+            currMax = max(num, currMax * num);
+            currMin = min(num, currMin * num);
+
+            maxProd = max(maxProd, currMax);
+        }
+
+        return maxProd;
     }
 };
