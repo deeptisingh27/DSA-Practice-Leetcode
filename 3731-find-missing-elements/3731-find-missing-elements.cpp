99822269d581957a1bb282ef2068b14dbc962893
Deepti Singh <deeptisingh2710@gmail.com>
2026-08-04 12:44:31 +0530

    Time: 0 ms (100%), Space: 32.4 MB (54.61%) - LeetHub

--- 3731-find-missing-elements/3731-find-missing-elements.cpp
+++ 3731-find-missing-elements/3731-find-missing-elements.cpp
@@ -2,19 +2,20 @@ class Solution {
 public:
     vector<int> findMissingElements(vector<int>& nums) {
 
+        /*
         //T.C = O(n + (maxi-mini+1)) , S.C = O(n) (excluding the output array)
-    
+
         unordered_set<int> numss(nums.begin(), nums.end()); //O(n)
 
-        /*
-        int mini = INT_MAX;
-        int maxi = INT_MIN;
+        
+        // int mini = INT_MAX;
+        // int maxi = INT_MIN;
 
-        for(int i=0 ; i<nums.size() ; i++){
-            if(nums[i] > maxi) maxi = nums[i];
-            if(nums[i] < mini) mini = nums[i];
-        }
-        */
+        // for(int i=0 ; i<nums.size() ; i++){
+        //     if(nums[i] > maxi) maxi = nums[i];
+        //     if(nums[i] < mini) mini = nums[i];
+        // }
+        
 
         int mini = *min_element(nums.begin(), nums.end()); //O(n)
         int maxi = *max_element(nums.begin(), nums.end()); //O(n)
@@ -23,6 +24,27 @@ public:
         for(int i=mini ; i<=maxi ; i++){ //O(maxi-mini+1)
             if(numss.find(i) == numss.end()){
                 ans.push_back(i);
+            }
+        }
+
+        return ans;
+        */
+
+
+        //T.C = O(n log n) , S.C = O(1)
+
+        int n = nums.size();
+
+        sort(nums.begin(), nums.end());
+
+        vector<int> ans;
+
+        // Iterate through adjacent pairs to detect missing ranges
+        for (int i=0 ; i<n-1 ; i++) {
+
+            // Fill all integer gaps between nums[i] and nums[i+1]
+            for (int val = nums[i]+1 ; val < nums[i+1]; val++) {
+                ans.push_back(val);
             }
         }
 
