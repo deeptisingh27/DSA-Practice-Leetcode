09e8748f99e4aed666a90a40a1e1e2646a28e85f
Deepti Singh <deeptisingh2710@gmail.com>
2026-08-19 23:10:29 +0530

    Time: 19 ms (80.74%), Space: 60.3 MB (68.07%) - LeetHub

--- 1386-cinema-seat-allocation/1386-cinema-seat-allocation.cpp
+++ 1386-cinema-seat-allocation/1386-cinema-seat-allocation.cpp
@@ -2,8 +2,8 @@ class Solution {
 public:
     int maxNumberOfFamilies(int n, vector<vector<int>>& reservedSeats) {
         //approach 1
-        //T.C = O(N) = S.C where N = reservedSeats.length
-        
+
+        /*
         unordered_map<int, unordered_set<int>> mp; //row -> reserved seats of that row
 
         for(auto & reservedSeat : reservedSeats){
@@ -30,6 +30,42 @@ public:
                 ans += 2;
 
             else if(grpA | grpB || grpC) 
+                ans += 1;            
+        }
+
+        return ans;
+        */
+
+
+        //approach 2
+
+        //row -> bitmask representing its reserved seats(10-bit integer)
+        unordered_map<int, int> mp; 
+
+        for(auto& reservedSeat : reservedSeats){
+            int row = reservedSeat[0];
+            int seat = reservedSeat[1];
+
+            mp[row] |= (1 << seat); //set the corresponding bit for the reserved seat
+        }
+
+        int ans = (n - mp.size()) * 2; //rows with no reserved seats can make grp A and C
+
+        // Bitmasks for the 3 grps using bitwise OR '|'
+        int maskA = (1<<2) | (1<<3) | (1<<4) | (1<<5);
+        int maskB = (1<<4) | (1<<5) | (1<<6) | (1<<7);
+        int maskC = (1<<6) | (1<<7) | (1<<8) | (1<<9);
+
+        for(auto& [row, bookedSeatsMask] : mp){
+
+            bool grpA = (bookedSeatsMask & maskA) == 0;
+            bool grpB = (bookedSeatsMask & maskB) == 0;
+            bool grpC = (bookedSeatsMask & maskC) == 0;
+
+            if(grpA && grpC) 
+                ans += 2;
+
+            else if(grpA | grpB | grpC) 
                 ans += 1;            
         }
 
