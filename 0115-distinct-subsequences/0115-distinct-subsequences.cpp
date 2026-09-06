7f0364ad8034513dfb37723e7c2d9c5ecd9c9ddc
Deepti Singh <deeptisingh2710@gmail.com>
2026-09-06 16:02:32 +0530

    Time: 31 ms (53.36%), Space: 13.1 MB (79.71%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -2,9 +2,10 @@ class Solution {
 public:
     int dp[1001][1001];
 
+    /*
     int solve(string &s, string &t, int i, int j){
-        if(j == t.length()) return 1;
-        if(i == s.length()) return 0;
+        if(j == t.length()) return dp[i][j] = 1;
+        if(i == s.length()) return dp[i][j] = 0;
 
         if (dp[i][j] != -1) return dp[i][j];
 
@@ -12,13 +13,35 @@ public:
             return dp[i][j] = solve(s, t, i+1, j+1) + solve(s, t, i+1, j);
         }
         else{
-            return dp[i][j] =    solve(s, t, i+1, j);
+            return dp[i][j] = solve(s, t, i+1, j);
+        }
+    }
+    */
+
+    int solvee(string &s, string &t, int m, int n){
+        if(n == 0) return dp[m][n] = 1; //found one subsequence
+        if(m == 0) return dp[m][n] = 0;
+
+        if (dp[m][n] != -1) return dp[m][n];
+
+        if(s[m-1] == t[n-1]){
+            return dp[m][n] = solvee(s, t, m-1, n-1) + solvee(s, t, m-1, n);
+        }
+        else{
+            return dp[m][n] =  solvee(s, t, m-1, n);
         }
     }
 
     int numDistinct(string s, string t) {
-        
+        /*
         memset(dp, -1, sizeof(dp));
         return solve(s, t, 0, 0);
+        */
+
+        //Knapsack (easier to convert to bottom up)
+        int m = s.length();
+        int n = t.length();
+        memset(dp, -1, sizeof(dp));
+        return solvee(s, t, m, n);
     }
 };
