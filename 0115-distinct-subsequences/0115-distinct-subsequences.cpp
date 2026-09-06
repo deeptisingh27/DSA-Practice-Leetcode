bdda2983e674f67631cd3eb5d4665f1d4908bf62
Deepti Singh <deeptisingh2710@gmail.com>
2026-09-06 16:18:13 +0530

    Time: 30 ms (58.87%), Space: 44.3 MB (20.6%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int dp[1001][1001];
+    // int dp[1001][1001];
 
     /*
     int solve(string &s, string &t, int i, int j){
@@ -18,8 +18,12 @@ public:
     }
     */
 
+    /*
     int solvee(string &s, string &t, int m, int n){
+        //base case 1
         if(n == 0) return dp[m][n] = 1; //found one subsequence
+
+        //base case 2
         if(m == 0) return dp[m][n] = 0;
 
         if (dp[m][n] != -1) return dp[m][n];
@@ -31,17 +35,56 @@ public:
             return dp[m][n] =  solvee(s, t, m-1, n);
         }
     }
+    */
 
     int numDistinct(string s, string t) {
+        //APPROACH 1
+        //top down(memo + recursion)
         /*
         memset(dp, -1, sizeof(dp));
         return solve(s, t, 0, 0);
         */
 
-        //Knapsack (easier to convert to bottom up)
+        //APPROACH 2
+        //Knapsack top down(easier to convert to bottom up)
+        /*
         int m = s.length();
         int n = t.length();
         memset(dp, -1, sizeof(dp));
         return solvee(s, t, m, n);
+        */
+
+        //APPROACH 3
+        //bottom up(using knapsack)
+        int m = s.length();
+        int n = t.length();
+
+        vector<vector<unsigned long long>> dp(m+1, vector<unsigned long long>(n+1)); //dp array
+
+        //base case 1 : dp[m][0] = 1
+        // if(n == 0) return 1;
+        for(int i=0 ; i<=m ; i++){
+            dp[i][0] = 1;
+        }
+
+        //base case 2 : dp[0][n] = 0
+        // if(m == 0) return 0;
+        for(int i=1 ; i<=n ; i++){
+            dp[0][i] = 0;
+        }
+
+        for(int i=1 ; i<=m ; i++){
+            for(int j=1 ; j<=n ; j++){
+
+                if(s[i-1] == t[j-1]){
+                    dp[i][j] = dp[i-1][j-1] + dp[i-1][j];
+                }
+                else{
+                    dp[i][j] = dp[i-1][j];
+                }
+            }
+        }
+
+        return dp[m][n];
     }
 };
