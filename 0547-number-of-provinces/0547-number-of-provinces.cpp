ad55e3bf29fe963e272b9d0f32b80942007273b7
Deepti Singh <deeptisingh2710@gmail.com>
2026-09-09 01:03:22 +0530

    Time: 4 ms (24.46%), Space: 23.9 MB (5.03%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -11,11 +11,29 @@ public:
         }
     }
 
-    int findCircleNum(vector<vector<int>>& isConnected) {
-        //DFS
-        int n = isConnected.size();
+    //khandaani BFS template
+    void BFS(unordered_map<int, vector<int>>& adj, int u, vector<bool>& visited){
+        queue<int> q;
 
-        vector<bool> visited(n, false);
+        q.push(u);
+        visited[u] = true;
+
+        while(!q.empty()){
+            int u = q.front();
+            q.pop();
+
+            for(int &v : adj[u]){
+                if(!visited[v]){
+                    BFS(adj, v, visited);
+                }                
+            }
+        }
+    }
+
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        //using DFS
+        /*
+        int n = isConnected.size();
 
         unordered_map<int, vector<int>> adj; //make adjacency list
 
@@ -28,10 +46,41 @@ public:
             }
         }
 
+        vector<bool> visited(n, false);
         int count = 0;
+
         for(int i=0 ; i<n ; i++){
             if(!visited[i]){
                 DFS(adj, i, visited);
+                count++;
+            }
+        }
+
+        return count;
+        */
+
+
+        //using BFS
+
+        int n = isConnected.size();
+
+        unordered_map<int, vector<int>> adj; //make adjacency list
+
+        for(int i=0 ; i<n ; i++){
+            for(int j=0 ; j<n ; j++){
+                if(isConnected[i][j] == 1){
+                    adj[i].push_back(j);
+                    adj[j].push_back(i);
+                }
+            }
+        }
+
+        vector<bool> visited(n, false);
+        int count = 0;
+
+        for(int i=0 ; i<n ; i++){
+            if(!visited[i]){
+                BFS(adj, i, visited);
                 count++;
             }
         }
