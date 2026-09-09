13381a26281b2c7012df095c0fec540217171706
Deepti Singh <deeptisingh2710@gmail.com>
2026-09-09 14:29:49 +0530

    Time: 0 ms (100%), Space: 8.9 MB (84.8%) - LeetHub

---
+++ 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    long long countCommas(long long n) {
+        if(n < 4) return 0;
+
+        long long commas = 0;
+
+        //each threshold represents where comma is introduced: 1,000 (1st comma), 1,000,000 (2nd comma), etc.
+        long long threshold = 1000;
+
+        while(n >= threshold){
+            commas += n - threshold + 1;
+            threshold *= 1000; //move to the next comma (1,000 -> 1,000,000)
+        }       
+
+        return commas;
+    }
+};
