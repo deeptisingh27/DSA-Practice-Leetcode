87812fcb42c5edb2adfdcd1a48d67caab016f2ee
Deepti Singh <deeptisingh2710@gmail.com>
2026-09-10 12:55:26 +0530

    Time: 0 ms (100%), Space: 15.6 MB (84.8%) - LeetHub

--- 2265-count-nodes-equal-to-average-of-subtree/2265-count-nodes-equal-to-average-of-subtree.cpp
+++ 2265-count-nodes-equal-to-average-of-subtree/2265-count-nodes-equal-to-average-of-subtree.cpp
@@ -36,14 +36,45 @@ public:
         solve(root->right, ans);        
     }
 
+    pair<int, int> solvee(TreeNode* root, int &ans){
+        if(!root) return {0, 0};
+
+        pair<int, int> P1 = solvee(root->left, ans);
+        pair<int, int> P2 = solvee(root->right, ans);
+
+        int totalSum = P1.first + P2.first + root->val;
+        int totalCount = P1.second + P2.second + 1;
+
+        int avg = totalSum / totalCount;
+
+        if(avg == root->val){
+            ans += 1;
+        }
+
+        return {totalSum, totalCount};
+    }
+
     int averageOfSubtree(TreeNode* root) {
         //Approach-1 Brute Force
         //T.C = O(n^2) for every root, we visit it's subtree to find average
         //S.C = O(1) (excluding recursion stack space)
 
+        /*
         int ans = 0;
         
         solve(root, ans);
+        
+        return ans;
+        */
+
+
+        //Approach-2 (Doing postorder traversal)
+        //T.C = O(n)
+        //S.C = O(1) (excluding recursion stack space)
+        
+        int ans = 0;
+        
+        solvee(root, ans);
         
         return ans;
     }
