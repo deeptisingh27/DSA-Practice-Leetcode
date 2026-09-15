35991b87dc527db25f2bb5da75c94581134aad18
Deepti Singh <deeptisingh2710@gmail.com>
2026-09-16 00:21:40 +0530

    Time: 0 ms (100%), Space: 42.1 MB (47.44%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,4 +1,5 @@
 class Solution {
+    /*
     private int solve(int m, int n, int[][] dp) {
         if (m == 1 || n == 1) return 1;
 
@@ -7,11 +8,40 @@ class Solution {
         // paths from top + paths from left
         return dp[m][n] = solve(m-1, n, dp) + solve(m, n-1, dp);
     }
+    */
 
     public int uniquePaths(int m, int n) {
         //TOP DOWN DP (recursion + memoization)
+        //T.C = O(m*n) = S.C
+
+        /*
         int[][] dp = new int[m+1][n+1];
 
         return solve(m, n, dp);
+        */
+
+        //BOTTOM UP DP (recursion + memoization)
+        //T.C = O(m*n) = S.C
+
+        int[][] dp = new int[m][n];
+
+        // only 1 way to reach any cell in the first column (all Down moves)
+        for (int i = 0; i < m; i++) {
+            dp[i][0] = 1;
+        }
+
+        // only 1 way to reach any cell in the first row (all Right moves)
+        for (int j = 0; j < n; j++) {
+            dp[0][j] = 1;
+        }
+
+        // Fill remaining cells 
+        for (int i=1 ; i<m ; i++) {
+            for (int j=1 ; j<n ; j++) {
+                dp[i][j] = dp[i-1][j] + dp[i][j-1];
+            }
+        }
+
+        return dp[m-1][n-1];
     }
 }
